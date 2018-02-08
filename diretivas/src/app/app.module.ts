24febff0bced1df446364a7f5c59d4be3a7d72d8
KRochas <karinerocha.s@hotmail.com>
2018-02-08 17:27:24 -0200

    ngswitch

--- diretivas/src/app/app.module.ts
+++ diretivas/src/app/app.module.ts
@@ -4,12 +4,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { DiretivaNgifComponent } from './diretiva-ngif/diretiva-ngif.component';
+import { DiretivaNgswitchComponent } from './diretiva-ngswitch/diretiva-ngswitch.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     DiretivaNgifComponent,
+    DiretivaNgswitchComponent,
   ],
   imports: [
     BrowserModule
