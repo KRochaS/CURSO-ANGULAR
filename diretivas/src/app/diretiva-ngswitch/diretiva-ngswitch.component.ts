5bfb614bfa0575b76c05f323fb92dfaae4ab507f
KRochas <karinerocha.s@hotmail.com>
2018-02-09 10:03:55 -0200

    ngSwitch, ngSwitchCase, ngSwitchDefault

--- diretivas/src/app/diretiva-ngswitch/diretiva-ngswitch.component.ts
+++ diretivas/src/app/diretiva-ngswitch/diretiva-ngswitch.component.ts
@@ -3,9 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-diretiva-ngswitch',
   templateUrl: './diretiva-ngswitch.component.html',
-  styleUrls: ['./diretiva-ngswitch.component.stylus']
+  styleUrls: ['diretiva-ngswitch.component.css']
 })
 export class DiretivaNgswitchComponent implements OnInit {
+
+  aba: string = "";
 
   constructor() { }
 
