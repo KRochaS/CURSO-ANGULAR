bc1ba5fc741f81424fcaec17c09257f483554260
KRochas <karinerocha.s@hotmail.com>
2018-02-15 13:29:24 -0200

    Rotas

---
+++ rotas/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { MaterializeModule } from 'angular2-materialize';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { CursosComponent } from './cursos/cursos.component';
+import { routing } from './app.routing';
+import { CursosService } from './cursos/cursos.service';
+import { CursoDetalheComponent } from './cursos/curso-detalhe/curso-detalhe.component';
+import { CursoNaoEncontradoComponent } from './cursos/curso-nao-encontrado/curso-nao-encontrado.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    CursosComponent,
+    CursoDetalheComponent,
+    CursoNaoEncontradoComponent
+  ],
+  imports: [
+    BrowserModule,
+    routing
+  ],
+  providers: [CursosService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
