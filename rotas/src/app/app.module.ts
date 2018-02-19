33181c26b1f7c8a6ed0b64ba2b11265c19369314
KRochas <karinerocha.s@hotmail.com>
2018-02-19 11:32:39 -0300

    Refatoração de código

--- rotas/src/app/app.module.ts
+++ rotas/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -6,11 +7,13 @@ import { AppComponent } from './app.component';
 import { MaterializeModule } from 'angular2-materialize';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
-import { CursosComponent } from './cursos/cursos.component';
-import { routing } from './app.routing';
-import { CursosService } from './cursos/cursos.service';
-import { CursoDetalheComponent } from './cursos/curso-detalhe/curso-detalhe.component';
-import { CursoNaoEncontradoComponent } from './cursos/curso-nao-encontrado/curso-nao-encontrado.component';
+// import { CursosComponent } from './cursos/cursos.component';
+// import { routing } from './app.routing';
+// import { CursosService } from './cursos/cursos.service';
+// import { CursoDetalheComponent } from './cursos/curso-detalhe/curso-detalhe.component';
+// import { CursoNaoEncontradoComponent } from './cursos/curso-nao-encontrado/curso-nao-encontrado.component';
+import { AppRoutingModule } from './app.routing.module';
+import { CursosModule } from './cursos/cursos.module';
 
 
 @NgModule({
@@ -18,15 +21,18 @@ import { CursoNaoEncontradoComponent } from './cursos/curso-nao-encontrado/curso
     AppComponent,
     HomeComponent,
     LoginComponent,
-    CursosComponent,
-    CursoDetalheComponent,
-    CursoNaoEncontradoComponent
+    // CursosComponent,
+    // CursoDetalheComponent,
+    // CursoNaoEncontradoComponent
   ],
   imports: [
     BrowserModule,
-    routing
+    FormsModule,
+    CursosModule,
+    AppRoutingModule
+ // routing
   ],
-  providers: [CursosService],
+  // providers: [CursosService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
